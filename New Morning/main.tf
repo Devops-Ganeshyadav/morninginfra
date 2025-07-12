d8cad0b8f3da6d025149aa9020fc80628a7d1631
Ganesh Yadav <ganesh4.yadav@outlook.com>
2025-07-12 17:14:04 +0530

    evening rg Deleted

--- New Morning/main.tf
+++ New Morning/main.tf
@@ -3,10 +3,7 @@ resource "azurerm_resource_group" "morning_rg" {
   location = var.location
 }
 
-resource "azurerm_resource_group" "evng_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
+
 
 resource "azurerm_storage_account" "morning_storage" {
     depends_on = [ azurerm_resource_group.morning_rg ]
